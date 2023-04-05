fe2af8a2eac8175094ae40422a6ce9a867f7d37c
sookdp <smdbdiop@gmail.com>
2023-04-05 13:20:51 +0200

    navbar

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuHamburger = document.querySelector(".menu-hamburger")
+        const navLinks = document.querySelector(".nav-links")
+
+        menuHamburger.addEventListener('click',()=>{
+            navLinks.classList.toggle('mobile-menu')})
